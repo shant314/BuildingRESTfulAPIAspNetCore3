467af58aca6ac856ba9e3200e80ca50b06e658a9
shant <shant@gmail.com>
2023-07-10 23:27:51 +0400

    03-Demo - Implementing the Outer Facing Contract (Part 1)

---
+++ BuildingRESTfulAPIAspNetCore3/Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using BuildingRESTfulAPIAspNetCore3.Infrastructure.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingRESTfulAPIAspNetCore3.Web.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorsController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var data = _authorRepository.GetAuthors();
+            return new JsonResult(data);
+        }
+    }
+}
